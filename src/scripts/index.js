0e7701f615a01b3b0a829c9c4bdd2e3ba0f87cb0
arifalhidayah <official.arif.alhidayah@gmail.com>
2024-03-21 13:41:54 +0700

    add responsive style

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,20 @@
-import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
+import '../styles/responsive.css'
 
-console.log('Hello Coders! :)');
+const menu = document.querySelector('#menu')
+const drawer = document.querySelector('#drawer');
+const main = document.querySelector('main');
+const hero = document.querySelector('.hero');
+
+menu.addEventListener('click', function (event) {
+    drawer.classList.toggle('open')
+    event.stopPropagation()
+})
+
+hero.addEventListener('click', () => {
+    drawer.classList.remove('open')
+});
+
+main.addEventListener('click', () => {
+    drawer.classList.remove('open')
+})
